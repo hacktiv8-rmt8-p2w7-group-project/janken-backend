ea87fa67fa079900c51410b2247d8ea57016d853
blackjac7 <febriansyah705@gmail.com>
2021-02-18 19:34:08 +0700

    add routes rooms

--- routers/router.js
+++ routers/router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
+const roomRouter = require('./room')
 const UserController = require('../controllers/UserController')
 
-
 router.post('/login', UserController.login)
+
+router.use('/rooms', roomRouter)
 
 
 module.exports = router
